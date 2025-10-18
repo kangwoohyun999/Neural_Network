df93bc123f00d6ff8c4385a2e287748ef0efe576
wh1012 <63694003+kangwoohyun999@users.noreply.github.com>
2025-10-18 22:55:12 +0900

    Delete Neural_Network/ch01-02 directory

--- Neural_Network/ch01-02/img_show.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-import os, matplotlib.pyplot as plt
-from matplotlib.image import imread
-
-img = imread(os.path.dirname(__file__)
-             + '/../dataset/cactus.png') # 이미지 읽어오기
-plt.imshow(img)
-
-plt.show()
